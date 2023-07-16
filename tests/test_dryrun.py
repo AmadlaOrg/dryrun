20e7ab07f5791e6f1e302276231c13849d80a472
jnbdz <237008+jnbdz@users.noreply.github.com>
2023-07-16 00:38:25 -0400

    Kept working on some tests.

--- tests/test_dryrun.py
+++ tests/test_dryrun.py
@@ -42,7 +42,9 @@ class TestDryRun(unittest.TestCase):
 
         # Additional tests to ensure the correct directory structure has been created
         dryrun_dir = os.path.join(self.temp_dir.name, '.dryrun', 'test')
+        #print(dryrun_dir)
         self.assertTrue(os.path.exists(dryrun_dir))  # Check that the .dryrun directory has been created
+        self.tearDown()  # Clean up the temporary directory after the test
 
     @patch('os.path.expanduser')
     def test_old_and_new_dirs_created(self, mock_expanduser):
@@ -53,6 +55,7 @@ class TestDryRun(unittest.TestCase):
         dryrun_dir = os.path.join(self.temp_dir.name, '.dryrun', 'test')
         self.assertTrue(os.path.exists(os.path.join(dryrun_dir, 'old')))
         self.assertTrue(os.path.exists(os.path.join(dryrun_dir, 'new')))
+        self.tearDown()
 
     @patch('os.path.expanduser')
     def test_config_toml_created(self, mock_expanduser):
@@ -69,6 +72,7 @@ class TestDryRun(unittest.TestCase):
         self.assertIn('time = "1"', content)
         self.assertIn('reboot = true', content)
         self.assertIn('strict = true', content)
+        self.tearDown()
 
     @patch('os.path.expanduser')
     def test_invalid_name_raises_error(self, mock_expanduser):
@@ -79,6 +83,38 @@ class TestDryRun(unittest.TestCase):
                                      '--strict'])
         self.assertNotEqual(result.exit_code, 0)
         self.assertIn('Name should only contain alphanumeric characters, underscores, and hyphens.', result.output)
+        self.tearDown()
+
+    @patch('os.path.expanduser')
+    def test_multiple_paths(self, mock_expanduser):
+        mock_expanduser.return_value = self.temp_dir.name
+        test_dirs = [
+            str(os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                             'fixtures', 'multiple_paths', 'dir1', 'etc', 'subject')),
+            str(os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                             'fixtures', 'multiple_paths', 'dir2', 'etc', 'subject'))]
+
+        #print(test_dirs)
+
+        result = self.runner.invoke(cli, ['setup', '--name', 'test', '--path', test_dirs[0], '--path', test_dirs[1],
+                                          '--time', '1', '--reboot', '--strict'])
+
+        # Print the result output to see the actual error
+        #print(result.output)
+
+        self.assertEqual(result.exit_code, 0)
+        dryrun_dir = os.path.join(self.temp_dir.name, '.dryrun', 'test')
+        #print(dryrun_dir)
+        config_toml = os.path.join(dryrun_dir, 'config.toml')
+        self.assertTrue(os.path.exists(config_toml))
+        with open(config_toml, 'r') as f:
+            content = f.read()
+        self.assertIn(f'paths = [\'{test_dirs[0]}\', \'{test_dirs[1]}\']', content)
+        print(os.path.abspath(os.path.join(dryrun_dir, 'old', 'dir1')))
+        print(os.path.exists(os.path.join(dryrun_dir, 'old', 'dir1')))
+        self.assertTrue(os.path.exists(os.path.join(dryrun_dir, 'old', 'dir1')))
+        #self.assertTrue(os.path.exists(os.path.join(dryrun_dir, 'old', 'dir2')))
+        self.tearDown()
 
 
 if __name__ == '__main__':
